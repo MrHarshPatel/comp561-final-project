2d83d81df8eea650ae851dd5339099557e87cdfd
Sebastian Ballesteros <tatanballesteros@hotmail.com>
2020-12-03 19:06:06 -0500

    Modified Genetic Algorithm to consensus sequence problem

--- GA.py
+++ GA.py
@@ -1,58 +1,61 @@
 '''
-Sebastian Ballesteros 
-This is a genetic algorithm used applied on a population of 10 citiies, each with an x and y
-coordinate. The goal of the algorithm is to solve the famous travelling salesman problem.
+Sebastian Ballesteros
+This is a genetic algorithm applied to the consensus sequence problem.
 '''
 
 '''
 Algorithm Definitions:
-population - array of individuals tuples, each with (x,y) coordinate
-order - array of randomly generate numbers which indicates  order to visit the cities
+population - array of candidate consensus sequences
 fitness - array of fitness values for each member of the population
-best_distance - best numerical distance found so far
-optimal_order - order for visiting cities which yields the best distance
+best_cost - best numerical cost found so far
+optimal_consensus - consensus sequence with the best cost found so far
 '''
 
 ############################## IMPORTS  ########################################
+
 import pandas
 import random
 import math
 import sys
+
 ########################## GLOBAL CONSTANTS ###################################
-POPULATION_SIZE = 0
-NUM_CITIES = 10
+
+POPULATION_SIZE = 4
+NUM_SEQUENCES = 4
 GENERATIONS = 0
+
 ########################### GLOBAL VARIABLES ##################################
-data_set = pandas.read_csv('cities.csv') #dataset is a 2D array with [coordinate][city]
-cities = []
+
+input_sequences = ['ACCTG','ACGAG','CCGCG','CCTGT']
 population = []
-order = [1,2,3,4,5,6,7,8,9,0]
 fitness = []
-best_distance = 100000000000 #very large number
-optimal_order = []
+best_cost = 100000000000 #very large number
+optimal_consensus = ''
+
 ###########################  HELPER FUNCTIONS ##################################
 
-def calculate_distance(order, cities):
-    total_distance = 0
-    for x in range(NUM_CITIES-1):
-        first_city = cities[order[x]]
-        second_city = cities [order[x+1]]
-        distance = math.hypot(second_city[0] - first_city[0],  second_city[1] - first_city[1])
-        total_distance += distance
-    return total_distance
+def calculate_edit_cost(candidate_consensus, sequence):
+    cost = 0
+    for i in range(0, len(sequence)):
+        # if nucleotides are the same in both sequences
+        if candidate_consensus[i] == sequence[i]:
+            cost += 0
+        # for now let's just assume 1 if they are not the same
+        elif candidate_consensus[i] != sequence[i]:
+            cost += 1
+    return cost
 
-def randomize(order, num):
-    for x in range (num):
-        #get 2 random numbers (indeces) for swapping
-        index_1 = random.randint(0,NUM_CITIES-1)
-        index_2 = random.randint(0,NUM_CITIES-1)
-        swap(order, index_1, index_2)
-    return order
+def calculate_consensus_score(candidate_consensus, sequences):
+    consensus_score = 0
+    for i in range(POPULATION_SIZE):
+        # Calculate the edit cost for each sequence in the the population
+        consensus_score += calculate_edit_cost(candidate_consensus, sequences[i])
+    return consensus_score
 
-def swap(order,i,j):
-    temp = order[i]
-    order[i] = order[j]
-    order[j] = temp
+def swap(sequence,i,j):
+    list_sequence = list(sequence)
+    list_sequence[i], list_sequence[j] = list_sequence[j], list_sequence[i]
+    return ''.join(list_sequence)
 
 def normalize_fitness(fitness):
     total_sum = 0
@@ -62,20 +65,20 @@ def normalize_fitness(fitness):
         fitness[i] = (fitness[i] / total_sum)
 
 def assign_fitness():
-    global best_distance
-    global optimal_order
+    global best_cost
+    global optimal_consensus
     global fitness
     fitness = []
     for i in range(POPULATION_SIZE):
-        #calcualte the distance between of a particular chromosome
-        element_distance = calculate_distance(population[i], cities)
-        #keep record of best distance and the optimal order while creating population
-        if element_distance < best_distance:
-            best_distance = element_distance
-            optimal_order = population[i]
-        #since we want to have a greater fitness value for a shorter distance
-        #we have to modify it, i.e the longer the distance the smaller the fitness value
-        fitness.append( 1 / float(calculate_distance(population[i], cities)))
+        #calcualte the edit cost of a particular chromosome
+        element_cost = calculate_consensus_score(population[i], input_sequences)
+        #keep record of element cost and the optimal consensus while creating population
+        if element_cost < best_cost:
+            best_cost = element_cost
+            optimal_consensus = population[i]
+        #since we want to have a greater fitness value for a smaller score
+        #we have to modify it, i.e the greater the score the smaller the fitness value
+        fitness.append( 1 / float(element_cost))
     normalize_fitness(fitness)
 
 def choose_parent(population, probabilities):
@@ -110,24 +113,16 @@ def endProgress():
 
 '''
 1. INITIAL POPULATION.
-    Read CSV file with the dataset information needed to create the cities
-    The population will consist of multiple arrays filled with random sequence
-    of numbers i.e [1,2,4,10, etc..] which is the order on which the cities are
-    going to be visited in
+    The initial population is the input strings
     For every element in the population, its fitness score its stored in the
     fitness array in the same index
 '''
 
 def populate():
     global population
-    #store the cities from the dataset
-    for i in range(NUM_CITIES):
-        cities.append( (data_set['x'][i], data_set['y'][i]) ) #append the tupple x,y
-        pass
     #initialize the population
-    for i in range(POPULATION_SIZE):
-        new_order = randomize(order, 5)
-        population.append( new_order.copy() )
+    for i in range(NUM_SEQUENCES):
+        population.append(input_sequences[i])
 
 ################################################################################
 
@@ -149,7 +144,7 @@ def reproduce():
         father = choose_parent(population,fitness)
         mother = choose_parent(population,fitness)
         child = crossover(father, mother)
-        mutate(child)
+        child = mutate(child)
         new_population.append(child)
     population = new_population
 
@@ -158,22 +153,18 @@ def reproduce():
 
 '''
 3. CROSSOVER
-    Crossover is implemented the following way: first by generating two random numbers
-    between 0 and 9 which will be the indeces taken from the father as [start, end],
-    later we will perfom crossover by taking the cities remained from the mother as long
-    as they don't already exist in the child's chromosome.
+    Crossover is implemented the following way: father will share he's DNA from
+    0 to a random number between 0 and the length of the sequence, mother then
+    complement with her remaining part
 '''
 
 def crossover(father, mother):
-    father_start = math.floor(random.uniform(0, NUM_CITIES-1))
-    father_end = math.floor(random.uniform(father_start+1, NUM_CITIES-1))
+    father_end = math.floor(random.uniform(0, len(input_sequences[0])-1))
     #create a new chromosome with the father's random DNA part
-    child = father[father_start:father_end+1]
+    child = father[0:father_end+1]
     #complement the new chromosomes' DNA with the mother's random DNA part
-    for i in range(NUM_CITIES):
-        #we need to make sure the DNA information is not already in the chromosome
-        if (mother[i] not in child):
-            child.append(mother[i])
+    for i in range(father_end+1, len(input_sequences[0])):
+        child += mother[i]
     return child
 
 ###############################################################################
@@ -181,14 +172,15 @@ def crossover(father, mother):
 '''
 4.MUTATION
     The implementation of mutation is really easy, just swap two random
-    generated cities to produce the new chromosome.
+    generated nucleotides in the sequence to produce the new chromosome.
 '''
 
-def mutate(order):
+def mutate(sequence):
     #randomly pick two items to swap
-    index_1 = random.randint(0,NUM_CITIES-1)
-    index_2 = random.randint(0,NUM_CITIES-1)
-    swap(order, index_1, index_2)
+    index_1 = random.randint(0, len(sequence)-1)
+    index_2 = random.randint(0, len(sequence)-1)
+    return swap(sequence, index_1, index_2)
+
 
 ###############################################################################
 
@@ -201,9 +193,7 @@ def mutate(order):
 
 def main():
     global GENERATIONS
-    global POPULATION_SIZE
     GENERATIONS = int(input("Number of generations desired (1000 recommended) : "))
-    POPULATION_SIZE = int(input("Population size desired (200 recommended) : "))
     startProgress('Generations')
     #set up the population
     populate()
@@ -214,8 +204,8 @@ def main():
         progress( i/GENERATIONS*100 )
     #end algorithm and print the result
     endProgress()
-    print('Best distance found: {}'.format(best_distance))
-    print('Order in which cities are visited: {}'.format(optimal_order))
+    print('Smaller cost found: {}'.format(best_cost))
+    print('Consensus sequence: {}'.format(optimal_consensus))
 
 
 if __name__ == "__main__":
